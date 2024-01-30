d2154f4ad1b8e9b2c18e3d94f3bedc11635ce3ad
madhan0923 <133847805+madhan0923@users.noreply.github.com>
2024-01-30 18:27:38 +0530

    Update split_var_call.R

--- R/split_var_call.R
+++ R/split_var_call.R
@@ -6,8 +6,8 @@
 #' @param max_length_out The maximum length of the output string
 #' @return A list with the split strings
 single_str_spilt <- function(result, item, sep, max_length_out) {
-  current <- tail(result, 1)
-  if (nchar(paste0(current, sep, item)) <= (max_length_out - 1)) {
+  current <- tail(result, 1L)
+  if (nchar(paste0(current, sep, item)) <= (max_length_out - 1L)) {
     result[length(result)] <- paste0(current, sep, item)
   } else {
     if (!identical(sep, " ")) result[length(result)] <- paste0(current, sep)
@@ -21,7 +21,7 @@ single_str_spilt <- function(result, item, sep, max_length_out) {
 #' @param string The string to be split
 #' @param max_length_out The maximum length of the output string (default is 200)
 #' @return A list with the split strings
-split_var <- function(string, max_length_out = 200) {
+split_var <- function(string, max_length_out = 200L) {
   # Pattern spot
   pattern <- names(which.max(table(stringr::str_extract_all(string, "[:punct:]|[:blank:]")))) %>%
     ifelse(is.null(.), "", .)
@@ -30,15 +30,15 @@ split_var <- function(string, max_length_out = 200) {
   split_vector <- unlist(stringr::str_split(string, pattern))
 
   # Use reduce to apply the function across the vector
-  split_vector <- split_vector[-1] %>%
-    purrr::reduce(single_str_spilt, .init = list(split_vector[1]), pattern, max_length_out) %>%
+  split_vector <- split_vector[-1L] %>%
+    purrr::reduce(single_str_spilt, .init = list(split_vector[1L]), pattern, max_length_out) %>%
     unlist()
 
   # Fix case where sentence do not exceed max_length_out
-  last_two <- paste0(tail(split_vector, n = 2), collapse = if (identical(pattern, " ")) " " else "")
+  last_two <- paste0(tail(split_vector, n = 2L), collapse = if (identical(pattern, " ")) " " else "")
   if (nchar(last_two) <= max_length_out) {
     split_vector <- c(
-      head(split_vector, n = -1),
+      head(split_vector, n = -1L),
       last_two
     )
   }
@@ -50,8 +50,8 @@ split_var <- function(string, max_length_out = 200) {
 #' @param domain_dataset The dataset to be split
 #' @param max_length_out The maximum length of output string (default is 200)
 #' @return A dataset with the split strings
-sdtm_str_split <- function(domain_dataset, max_length_out = 200) {
-  out_n <- outt <- NULL
+sdtm_str_split <- function(domain_dataset, max_length_out = 200L) {
+  outt <- NULL
 
   # Filtering columns > 200
   char_200 <- dplyr::select_if(domain_dataset, ~ max(nchar(.)) >= max_length_out)
@@ -70,6 +70,6 @@ sdtm_str_split <- function(domain_dataset, max_length_out = 200) {
     dplyr::bind_cols()
 
   names(outt) <- sub("....$", "", names(outt)) %>% make.unique(., sep = "_")
-  dataset_OUT <- dplyr::bind_cols(dplyr::select(domain_dataset, -names(char_200)), outt)
-  return(dataset_OUT)
+  dataset_out <- dplyr::bind_cols(dplyr::select(domain_dataset, -names(char_200)), outt)
+  return(dataset_out)
 }
