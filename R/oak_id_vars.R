e87ca66c72ef6fa2b30cbd4e85be09d7aa4b9852
Ramiro Magno <rmagno@pattern.institute>
2024-03-24 17:17:25 +0000

    Add `contains_oak_id_vars()` function

--- R/oak_id_vars.R
+++ R/oak_id_vars.R
@@ -24,3 +24,32 @@ oak_id_vars <- function(extra_vars = NULL) {
   unique(c("oak_id", "raw_source", "patient_number", extra_vars))
 
 }
+
+#' Does a vector contain the raw dataset key variables?
+#'
+#' [contains_oak_id_vars()] evaluates whether a character vector `x` contains
+#' the raw dataset key variable names, i.e. the so called Oak identifier
+#' variables --- these are defined by the return value of [oak_id_vars()].
+#'
+#' @param x A character vector.
+#'
+#' @returns A logical scalar value.
+#'
+#' @examples
+#' # `oak_id_vars()` is the function that defines what are the minimal set of
+#' # oak keys. Hence, by definition, the following code should always return
+#' # `TRUE`.
+#' sdtm.oak:::contains_oak_id_vars(oak_id_vars())
+#'
+#' # Returns `FALSE`.
+#' sdtm.oak:::contains_oak_id_vars(character())
+#'
+#' # Another example that returns `FALSE` because it is missing
+#' # `"patient_number"`.
+#' sdtm.oak:::contains_oak_id_vars(c("oak_id", "raw_source"))
+#'
+#' @keywords internal
+contains_oak_id_vars <- function(x) {
+  admiraldev::assert_character_vector(x)
+  all(oak_id_vars() %in% x)
+}
