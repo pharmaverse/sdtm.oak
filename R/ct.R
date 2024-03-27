0eb467797a14621082b8c5e4a2f8731d11454966
Ramiro Magno <rmagno@pattern.institute>
2024-03-27 21:50:48 +0000

    Add functions ct importing
    - Adds three new user facing ct-related functions: `read_ct_example()`, `ct_example()` and `read_ct()`
    - Provides a ct example file in inst/ct/

--- R/ct.R
+++ R/ct.R
@@ -1,3 +1,57 @@
+#' Controlled terminology variables
+#'
+#' @description
+#' [ct_vars()] returns the mandatory variables to be present in a data set
+#' representing a controlled terminology. By default, it returns all required
+#' variables.
+#'
+#' If only the subset of variables used for matching terms are needed, then
+#' request this subset of variables by passing the argument value `"from"`. If
+#' only the mapping-to variable is to be requested, then simply pass `"to"`. If
+#' only the code-list code variable name is needed then pass `"cl"`.
+#'
+#' @param set A scalar character (string), one of: `"all"` (default), `"cl"`,
+#'   `"from"` or `"to"`.
+#'
+#' @examples
+#' # These two calls are equivalent and return all required variables in a
+#' # controlled terminology data set.
+#' sdtm.oak:::ct_vars()
+#' sdtm.oak:::ct_vars("all")
+#'
+#' # "Codelist code" variable name.
+#' sdtm.oak:::ct_vars("cl")
+#'
+#' # "From" variables
+#' sdtm.oak:::ct_vars("from")
+#'
+#' # The "to" variable.
+#' sdtm.oak:::ct_vars("to")
+#'
+#' @keywords internal
+ct_vars <- function(set = c("all", "cl", "from", "to")) {
+
+  admiraldev::assert_character_vector(set)
+
+  set <- match.arg(set)
+  cl_var <- "codelist_code"
+  from_vars <- c("collected_value", "term_synonyms")
+  to_var <- "term_value"
+
+  if (identical(set, "all"))
+    return(c(cl_var, from_vars, to_var))
+
+  if (identical(set, "cl"))
+    return(cl_var)
+
+  if (identical(set, "from"))
+    return(from_vars)
+
+  if (identical(set, "to"))
+    return(to_var)
+
+}
+
 #' Controlled terminology mappings
 #'
 #' @description
@@ -32,7 +86,7 @@
 #'
 #' @importFrom rlang .data
 #' @keywords internal
-ct_mappings <- function(ct, from =  c("collected_value", "term_synonyms"), to = "term_value") {
+ct_mappings <- function(ct, from = ct_vars("from"), to = ct_vars("to")) {
 
   # TODO: Assertions and memoisation.
 
@@ -85,13 +139,13 @@ ct_map <-
   function(x,
            ct = NULL,
            cl = NULL,
-           from =  c("collected_value", "term_synonyms"),
-           to = "term_value") {
+           from =  ct_vars("from"),
+           to = ct_vars("to")) {
 
     ct %||% return(x)
 
-    cl <- cl %||% unique(ct$codelist_code)
-    ct <- dplyr::filter(ct, .data$codelist_code %in% cl)
+    cl <- cl %||% unique(ct[[ct_vars("cl")]])
+    ct <- dplyr::filter(ct, .data[[ct_vars("cl")]] %in% cl)
 
     mappings <- ct_mappings(ct, from = from, to = to)
     recode(
@@ -102,3 +156,119 @@ ct_map <-
     )
 
   }
+
+#' Read in a controlled terminology
+#'
+#' [read_ct()] imports a controlled terminology specification data set as a
+#' [tibble][tibble::tibble-package].
+#'
+#' @param file A path to a file containing a controlled terminology
+#'   specification data set. The following are expected of this file:
+#'
+#' - The file is expected to be a CSV file;
+#' - The file is expected to contain a first row of column names;
+#' - This minimal set of variables is expected: `r knitr::combine_words(ct_vars())`.
+#'
+#' @returns A [tibble][tibble::tibble-package] with a controlled terminology
+#'   specification.
+#'
+#' @export
+read_ct <- function(file = stop("`file` must be specified")) {
+
+  # TODO: Until we have a more precise specification of the controlled
+  # terminology, we just read all columns as character. We assert nonetheless
+  # for the existence of the ct variables: `collected_value`, `term_synonyms` and
+  # `term_value` (provided by the helper `ct_vars()`).
+  ct <- readr::read_csv(file = file, col_types = "c")
+  admiraldev::assert_data_frame(arg = ct, required_vars = rlang::syms(ct_vars()))
+
+  ct
+}
+
+#' Find the path to an example controlled terminology file
+#'
+#' [ct_example()] resolves the local path to an example controlled
+#' terminology file.
+#'
+#' @param example A string with either the basename, file name, or relative path
+#'   to a controlled terminology file bundled with `{stdm.oak}`, see examples.
+#'
+#'
+#' @examples
+#' # Get the local path to controlled terminology example file 01
+#' # Using the basename only:
+#' ct_example("ct-01-cm")
+#'
+#' # Using the file name:
+#' ct_example("ct-01-cm.csv")
+#'
+#' # Using the relative path:
+#' ct_example("ct/ct-01-cm.csv")
+#'
+#' # If no example is provided it returns a vector of possible choices.
+#' ct_example()
+#'
+#' @export
+ct_example <- function(example) {
+  # If no example is requested, then return all available files.
+  if (missing(example)) {
+    ct_path <- system.file("ct", package = "sdtm.oak", mustWork = TRUE)
+    ct_files <- list.files(ct_path, pattern = "*.csv")
+    return(ct_files)
+  }
+
+  # Otherwise, resolve the local path to the example requested.
+  admiraldev::assert_character_scalar(example, optional = TRUE)
+  base_name <- tools::file_path_sans_ext(basename(example))
+  path <- file.path("ct", paste0(base_name, ".csv"))
+  local_path <- system.file(path, package = "sdtm.oak")
+
+  if (identical(local_path, "")) {
+    stop(
+      glue::glue(
+        "'{example}' does not match any ct files. Run `ct_example()` for options."
+      ),
+      call. = FALSE
+    )
+
+  } else {
+    local_path <-
+      system.file(path, package = "sdtm.oak", mustWork = TRUE)
+    return(local_path)
+  }
+}
+
+#' Read an example controlled terminology specification
+#'
+#' [read_ct_example()] imports one of the bundled controlled terminology
+#' specification data sets as a [tibble][tibble::tibble-package] into R.
+#'
+#' @param example The file name of a controlled terminology data set bundled
+#'   with `{stdm.oak}`, run `read_ct_example()` for available example files.
+#'
+#' @returns A [tibble][tibble::tibble-package] with a controlled terminology
+#'   specification data set.
+#'
+#' @examples
+#' # Leave the `example` parameter as missing for available example files.
+#' read_ct_example()
+#'
+#' # Read an example ct file.
+#' read_ct_example("ct-01-cm.csv")
+#'
+#' # You may omit the file extension.
+#' read_ct_example("ct-01-cm")
+#'
+#' @export
+read_ct_example <- function(example) {
+
+  if (missing(example)) {
+    return(ct_example())
+  } else {
+    admiraldev::assert_character_scalar(example)
+  }
+
+  path <- ct_example(example)
+  read_ct(file = path)
+
+}
---
+++ inst/ct/README.md
@@ -0,0 +1,22 @@
+# Controlled terminology examples
+
+## Introduction
+
+The folder inst/ct is meant to hold examples of controlled terminology
+specifications. These example files should be plain CSV files.
+
+If you're adding new example files please follow the naming convention:
+`ct-<DD>-<keyword>.csv`, where `<DD>` is a simple numerical identifier
+for the example, i.e. 01, 02, etc.. The `<keyword>` is a short keyword
+providing a simple contextual annotation for the controlled terminology.
+
+For example, `ct-01-cm.csv` is an example of a controlled terminology
+specification data set:
+
+- `01`: indicates that it is the first example
+- `cm`: suggests that this data set is related to the Concomitant Medication
+domain.
+
+## Examples
+
+- `ct-01-cm.csv`: TBD.
