b8a2e20ad1fd1708ae2bb81bf795e614f214ef4f
Ramiro Magno <rmagno@pattern.institute>
2024-05-15 00:22:40 +0100

    Reformating of docs of `derive_seq()`

--- R/derive_seq.R
+++ R/derive_seq.R
@@ -5,9 +5,9 @@
 #' (`--SEQ`).
 #'
 #' This function adds a newly derived variable to `tgt_dat`, namely the sequence
-#' number (`--SEQ`) whose name is `tgt_var`. An integer sequence is generated
-#' that, when used in combination with the identifier of the subject, uniquely
-#' identifies each record within the domain.
+#' number (`--SEQ`) whose name is the one provided in `tgt_var`. An integer
+#' sequence is generated that, when used in combination with the identifier of
+#' the subject, uniquely identifies each record within the domain.
 #'
 #' If `tgt_dat` pertains a domain that does not contain subject data, then
 #' passing `NULL` to `vars_id` will ensure that every row in `tgt_dat` gets a
@@ -15,13 +15,13 @@
 #'
 #' @param tgt_dat The target dataset, a data frame.
 #' @param tgt_var The target SDTM variable: a single string indicating the name
-#'  of the sequence number (`--SEQ`) variable, e.g. `"DSSEQ"`. Note that supplying
-#'  a name not ending in `"SEQ"` will raise a warning.
+#'   of the sequence number (`--SEQ`) variable, e.g. `"DSSEQ"`. Note that
+#'   supplying a name not ending in `"SEQ"` will raise a warning.
 #' @param id_vars Either a character vector of identifier variables or `NULL`.
 #'   Default is the set of variables returned by [oak_id_vars()].
 #'
 #' @returns Returns the data frame supplied in `tgt_dat` with the newly derived
-#'   variable, i.e. the sequence number (`--SEQ`) whose name is that passed in
+#'   variable, i.e. the sequence number (`--SEQ`), whose name is that passed in
 #'   `tgt_var`. This variable is of type integer.
 #'
 #' @examples
