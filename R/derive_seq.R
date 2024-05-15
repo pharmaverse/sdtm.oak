8662fa713175a3eeeeb6dcfc64e82093e5213dc8
Ramiro Magno <rmagno@pattern.institute>
2024-05-15 21:37:11 +0100

    Update `derive_seq()`
    - Two examples of domain data sets were added: vs and apsc
    - An internal data set `domain_record_vars` provides the mapping between domain names and key variables (thank you @edgar-manukyan)
    - `rec_vars()` was added to access those record-defining variables
    - Likewise, `sbj_vars()` for subject-level defining rows

--- R/derive_seq.R
+++ R/derive_seq.R
@@ -9,39 +9,59 @@
 #' sequence is generated that uniquely identifies each record within the domain.
 #'
 #' Prior to the derivation of `tgt_var`, the data frame `tgt_dat` is sorted
-#' according to grouping variables indicated in `id_vars`.
+#' according to grouping variables indicated in `rec_vars`.
 #'
 #' @param tgt_dat The target dataset, a data frame.
 #' @param tgt_var The target SDTM variable: a single string indicating the name
 #'   of the sequence number (`--SEQ`) variable, e.g. `"DSSEQ"`. Note that
 #'   supplying a name not ending in `"SEQ"` will raise a warning.
-#' @param id_vars Either a character vector of identifier variables.
-#'   Default is the set of variables returned by [oak_id_vars()].
+#' @param rec_vars A character vector of record-level identifier variables. Use
+#' [rec_vars()] to quickly get the identifier variables for a given domain.
+#' @param sbj_vars A character vector of subject-level identifier variables.
 #' @param start_at The sequence numbering starts at this value (default is `1`).
 #'
 #' @returns Returns the data frame supplied in `tgt_dat` with the newly derived
 #'   variable, i.e. the sequence number (`--SEQ`), whose name is that passed in
 #'   `tgt_var`. This variable is of type integer.
 #'
+#' @examples
+#' # A VS raw data set example
+#' (vs <- read_domain_example("vs"))
+#'
+#' # Derivation of VSSEQ
+#' derive_seq(tgt_dat = vs, tgt_var = "VSSEQ", rec_vars = rec_vars("VS"))
+#'
+#' # An APSC raw data set example
+#' (apsc <- read_domain_example("apsc"))
+#'
+#' # Derivation of APSEQ
+#' derive_seq(
+#'   tgt_dat = apsc,
+#'   tgt_var = "APSEQ",
+#'   rec_vars = c("STUDYID", "RSUBJID", "SCTESTCD"),
+#'   sbj_vars = c("STUDYID", "RSUBJID")
+#' )
 #' @export
 derive_seq <-
   function(tgt_dat,
            tgt_var,
-           id_vars,
+           rec_vars,
+           sbj_vars = sdtm.oak::sbj_vars(),
            start_at = 1L) {
     admiraldev::assert_character_scalar(tgt_var)
 
-    admiraldev::assert_character_vector(id_vars)
+    admiraldev::assert_character_vector(rec_vars)
+    admiraldev::assert_character_vector(sbj_vars)
     admiraldev::assert_data_frame(tgt_dat,
-                                  required_vars = rlang::syms(id_vars),
+                                  required_vars = rlang::syms(rec_vars),
                                   optional = FALSE)
 
     admiraldev::assert_integer_scalar(start_at, subset = "non-negative")
 
     tgt_dat |>
       dplyr::ungroup() |> # ensure that is ungrouped
-      dplyr::arrange(dplyr::across(.cols = dplyr::all_of(id_vars))) |>
-      dplyr::group_by(dplyr::across(dplyr::all_of(id_vars))) |>
+      dplyr::arrange(dplyr::across(.cols = dplyr::all_of(rec_vars))) |>
+      dplyr::group_by(dplyr::across(dplyr::all_of(sbj_vars))) |>
       dplyr::mutate("{tgt_var}" := dplyr::row_number() + start_at - 1L) |>
       dplyr::ungroup()
   }
---
+++ data-raw/roak_ex.R
@@ -0,0 +1,31 @@
+# Title: `roak` example data sets
+# Description: Data sets provided during the initial development of sdtm.oak
+# by the team at Roche.
+
+library(readr)
+library(here)
+
+path <- here::here("inst/domain")
+
+vs <- tibble::tribble(
+  ~STUDYID, ~DOMAIN,      ~USUBJID,                  ~VSSPID, ~VSTESTCD,             ~VSDTC, ~VSTPTNUM,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS1-D:9795532-R:2",   "DIABP", "2020-09-01T13:31",        NA,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS1-D:9795532-R:2",    "TEMP", "2020-09-01T13:31",        NA,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS2-D:9795533-R:2",   "DIABP", "2020-09-28T11:00",         2,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS2-D:9795533-R:2",    "TEMP", "2020-09-28T11:00",         2,
+  "ABC123",    "VS",  "ABC123-376", "/F:VTLS1-D:9795591-R:1",   "DIABP",       "2020-09-20",        NA,
+  "ABC123",    "VS",  "ABC123-376", "/F:VTLS1-D:9795591-R:1",    "TEMP",       "2020-09-20",        NA
+)
+
+apsc <- tibble::tribble(
+  ~STUDYID, ~RSUBJID,     ~SCTESTCD,  ~DOMAIN, ~SREL,     ~SCCAT,
+  "ABC123", "ABC123-210", "LVSBJIND", "APSC",  "FRIEND",  "CAREGIVERSTUDY",
+  "ABC123", "ABC123-210", "EDULEVEL", "APSC",  "FRIEND",  "CAREGIVERSTUDY",
+  "ABC123", "ABC123-210", "TMSPPT",   "APSC",  "FRIEND",  "CAREGIVERSTUDY",
+  "ABC123", "ABC123-211", "CAREDUR",  "APSC",  "SIBLING", "CAREGIVERSTUDY",
+  "ABC123", "ABC123-211", "LVSBJIND", "APSC",  "SIBLING", "CAREGIVERSTUDY",
+  "ABC123", "ABC123-212", "JOBCLAS",  "APSC",  "SPOUSE",  "CAREGIVERSTUDY"
+)
+
+readr::write_rds(x = vs, file = file.path(path, "vs.rds"), compress = "xz")
+readr::write_rds(x = apsc, file = file.path(path, "apsc.rds"), compress = "xz")
