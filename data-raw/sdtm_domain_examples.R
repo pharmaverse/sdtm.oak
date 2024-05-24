25828c4ada31a37a503aae50c6cf8a29338e19fb
Ramiro Magno <rmagno@pattern.institute>
2024-05-24 01:08:45 +0100

    Linting issues (still)

--- data-raw/sdtm_domain_examples.R
+++ data-raw/sdtm_domain_examples.R
@@ -8,12 +8,12 @@ path <- here::here("inst/domain")
 
 vs <- tibble::tribble(
   ~STUDYID, ~DOMAIN,      ~USUBJID,                  ~VSSPID, ~VSTESTCD,             ~VSDTC, ~VSTPTNUM,
-  "ABC123",    "VS",  "ABC123-375", "/F:VTLS1-D:9795532-R:2",   "DIABP", "2020-09-01T13:31",        NA,
-  "ABC123",    "VS",  "ABC123-375", "/F:VTLS1-D:9795532-R:2",    "TEMP", "2020-09-01T13:31",        NA,
-  "ABC123",    "VS",  "ABC123-375", "/F:VTLS2-D:9795533-R:2",   "DIABP", "2020-09-28T11:00",         2,
-  "ABC123",    "VS",  "ABC123-375", "/F:VTLS2-D:9795533-R:2",    "TEMP", "2020-09-28T11:00",         2,
-  "ABC123",    "VS",  "ABC123-376", "/F:VTLS1-D:9795591-R:1",   "DIABP",       "2020-09-20",        NA,
-  "ABC123",    "VS",  "ABC123-376", "/F:VTLS1-D:9795591-R:1",    "TEMP",       "2020-09-20",        NA
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS1-D:9795532-R:2", "DIABP", "2020-09-01T13:31",  NA,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS1-D:9795532-R:2", "TEMP",  "2020-09-01T13:31",  NA,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS2-D:9795533-R:2", "DIABP", "2020-09-28T11:00",  2L,
+  "ABC123",    "VS",  "ABC123-375", "/F:VTLS2-D:9795533-R:2", "TEMP",  "2020-09-28T11:00",  2L,
+  "ABC123",    "VS",  "ABC123-376", "/F:VTLS1-D:9795591-R:1", "DIABP", "2020-09-20",        NA,
+  "ABC123",    "VS",  "ABC123-376", "/F:VTLS1-D:9795591-R:1", "TEMP",  "2020-09-20",        NA
 )
 
 apsc <- tibble::tribble(
@@ -30,4 +30,3 @@ readr::write_rds(x = pharmaversesdtm::ae, file = file.path(path, "ae.rds"), comp
 readr::write_rds(x = pharmaversesdtm::cm, file = file.path(path, "cm.rds"), compress = "xz")
 readr::write_rds(x = vs, file = file.path(path, "vs.rds"), compress = "xz")
 readr::write_rds(x = apsc, file = file.path(path, "apsc.rds"), compress = "xz")
-
