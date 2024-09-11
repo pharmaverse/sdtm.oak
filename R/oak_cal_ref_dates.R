9c90a71175f770d1a02de4fd4b9390c79f547d79
muzzama-1990 <mohsin.uzzama2@gmail.com>
2024-09-11 17:23:21 +0100

    Remove comments

--- R/oak_cal_ref_dates.R
+++ R/oak_cal_ref_dates.R
@@ -7,44 +7,7 @@ oak_cal_ref_dates <- function(ds_in = dm,
                              min_max = "min",
                              raw_dataset_list,
                              raw_date_list) {
-  # current_function()
 
-  # validate the input
-  # oak_assert_that(is.data.frame(ds_in))
-  # # oak_assert_that(
-  # #   der_var == "RFSTDTC",
-  # #   msg = "der_var must be equal to 'RFSTDTC'"
-  # # )
-  # oak_assert_that(unique(ds_in$DOMAIN) == "DM",
-  #                 msg = "ds_in must contain only 'DM' domain"
-  # )
-  # oak_assert_that(
-  #   !(der_var %in% colnames(ds_in)),
-  #   msg = paste(
-  #     "ds_in data frame must not contain",
-  #     der_var,
-  #     "column"
-  #   )
-  # )
-  # oak_assert_that(is.list(raw_dataset_list))
-  # oak_assert_that(length(raw_dataset_list) > 0)
-  #
-  # oak_assert_that(is.list(raw_var_list))
-  # oak_assert_that(length(raw_var_list) > 0)
-  #
-  # # Check if the raw_var_list has equal count of raw_dataset_list
-  # oak_assert_that(length(raw_dataset_list) == length(raw_var_list))
-
-  # check if ref_date_conf file is present in the oak_pkg_env
-  # oak_assert_that(
-  #   "ref_date_conf" %in% ls(oak_pkg_env),
-  #   msg = paste(
-  #     "'ref_date_conf' not present in the 'oak_pkg_env'.",
-  #     "Provide reference_date_configuration.csv."
-  #   )
-  # )
-
-  # ref_date_conf_loaded <- oak_pkg_env$ref_date_conf
   ds_out <- data.frame()
   for(i in 1:length(raw_dataset_list)) {
 browser()
