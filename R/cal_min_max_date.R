92161076b5e439f159da703611996b365e988ae8
muzzama-1990 <mohsin.uzzama2@gmail.com>
2024-09-18 16:40:28 +0100

    updates

--- R/cal_min_max_date.R
+++ R/cal_min_max_date.R
@@ -1,42 +1,85 @@
-#' Populate RFSTDTC variable in demographic domain in ISO8601 character format.
+#' Calculate minimum and maximum date and time in the dataframe
 #'
-#' Derive RFSTDTC based on the configuration file.
+#' @description This function derives the earliest/latest ISO8601 datetime
 #'
-#' @details
+#' @param raw_dataset Raw source data frame
+#' @param date_variable Single character string. Name of the date variable
+#' @param time_variable Single character string. Name of the time variable
+#' @param val_type Single character string determining whether to look
+#'   for the earliest or the latest datetime combination. Permitted values:
+#'   "min", "max". Default to "min".
+#' @param date_format Format of source date variable
+#' @param time_format Format of source time variable
 #'
-#' Calculate minimum or maximum dates
-
+#' @return Data frame with 2 columns: unique subject and datetime variable
+#'   column storing the earliest/latest datetime.
+#'
+#' @export
+#' @examples
+#' EX <- tibble::tribble(
+#'   ~patient_number,    ~EX_ST_DT,      ~EX_EN_DT,   ~EX_ST_TM,
+#'             "001", "26-10-1990",   "10-01-1985",     "10:20",
+#'             "001", "26-10-1990",   "10-01-1985",     "10:15",
+#'             "001", "26-10-1990",   "10-01-1985",     "10:19",
+#'             "002", "26-10-1991",             NA,   "UNK:UNK"
+#'             )
+#'
+#'cal_min_max_date(EX, "EX_ST_DT",
+#'                 "EX_ST_TM", date_format = "dd-mmm-yyyy",
+#'                 time_format = "H:M")
+#'
 cal_min_max_date <- function(raw_dataset,
-                              sdtm_var_name,
-                              date_variable,
-                              val_type = "min"
+                             date_variable,
+                             time_variable,
+                             val_type = "min",
+                             date_format,
+                             time_format
 ) {
-  browser()
-  # Check if date and time variable are present in the raw dataset
+
+  # Check if date is present in the raw data frame
   date_not_in_data <- !(date_variable %in% colnames(raw_dataset))
 
-  if (date_not_in_data) {
-    # Return Empty Dataset with SUBJID and sdtm_var_name
-    final_df <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("patient_number", "datetime"))
-    return(final_df)
+  # Check if time variable is used and if present in the raw data frame
+  time_not_in_data <- !(time_variable %in% colnames(raw_dataset)) && !is.na(time_variable)
+
+  # If both date and time variables are not present return the empty data frame
+  if (date_not_in_data || time_not_in_data) {
+    # Return Empty data frame with patient_number and datetime columns
+    empty_df <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("patient_number", "datetime"))
+    cli::cli_warn(paste("Date variable",date_variable, "or Time variable", time_variable,
+                         "not present in source data"))
+    return(empty_df)
   }
 
   final_df <- raw_dataset
+  # Time variable is not used then use only date
+  if (is.na(time_variable)) {
+    final_df$datetime <- create_iso8601(raw_dataset[[date_variable]],
+                                        .format = date_format)
+  } else {
+    # If both date and time variables are presen use both date and time
+    raw_dataset$date_time <- paste0(raw_dataset[[date_variable]],
+                                    raw_dataset[[time_variable]])
+    format = paste0(date_format,time_format)
 
-  final_df$datetime <- create_iso8601(raw_dataset[[date_variable]], .format = "dd-mm-yyyy")
+    final_df$datetime <- create_iso8601(raw_dataset$date_time,
+                                        .format = format,
+                                        .na = c("UNK", "NA", "U","unk", "u", "un", "UNK"))
+  }
 
   final_df <- final_df |>
-    dplyr::select(c("SUBJID", "datetime"))|> unique()
+    dplyr::select(c("patient_number", "datetime"))|>
+    unique()
 
   final_df <- final_df |>
     dplyr::mutate(date_time = datetime) |>
     tidyr::separate(
       date_time,
       sep = "-|T|:",
-      into = c("year", "month", "day"), #, "hour", "minute"
+      into = c("year", "month", "day", "hour", "minute"),
       fill = "right",
       extra = "drop"
-    )|>
+      )|>
     list() |>
     setNames("x") |>
     with(replace(x, x == "UNK", NA)) |>
@@ -44,21 +87,18 @@ cal_min_max_date <- function(raw_dataset,
     setNames("x") |>
     with(replace(x, x == "", NA))
 
-
   if (val_type == "min") {
     final_df1 <- final_df |>
-      dplyr::arrange(year, month, day)#, hour, minute)
+      dplyr::arrange(year, month, day, hour, minute)
   } else {
     final_df1 <- final_df |>
-      dplyr::arrange(dplyr::desc(year), dplyr::desc(month), dplyr::desc(day))#, dplyr::desc(hour), dplyr::desc(minute))
+      dplyr::arrange(dplyr::desc(year), dplyr::desc(month), dplyr::desc(day), dplyr::desc(hour), dplyr::desc(minute))
   }
 
   # Keep first appearance in the data frame since it is already sorted
-  final_df2 <- final_df1[!duplicated(final_df1$SUBJID), c("SUBJID", "datetime")]
+  final <- final_df1[!duplicated(final_df1$patient_number), c("patient_number", "datetime")]
 
-  final_df2 <- final_df2 |> dplyr::filter(!is.na(datetime))
+  final <- final |> dplyr::filter(!is.na(datetime))
 
-  # colnames(final_df2)[colnames(final_df2) == "datetime"] <- sdtm_var_name
-
-  return(final_df2)
+  return(final)
 }
--- R/oak_cal_ref_dates.R
+++ R/oak_cal_ref_dates.R
@@ -5,34 +5,45 @@
 oak_cal_ref_dates <- function(ds_in = dm,
                              der_var = "RFSTDTC",
                              min_max = "min",
-                             raw_dataset_list,
-                             raw_date_list) {
+                             ref_date_conf,
+                             raw_source) {
 
   ds_out <- data.frame()
-  for(i in 1:length(raw_dataset_list)) {
+  for(i in 1:length(ref_date_conf$dataset_name)) {
 browser()
-    raw_dataset <- raw_source[[raw_dataset_list[i]]]
-    date_variable <- raw_date_list[[i]]
+    raw_dataset_name <- ref_date_conf$dataset_name[i]
+    date_variable <- ref_date_conf$date_var[i]
+    date_format <- ref_date_conf$dformat[i]
+    time_var <- ref_date_conf$time_var[i]
+    time_format <- ref_date_conf$tformat[i]
+    sdtm_var <- ref_date_conf$sdtm_var_name[i]
+    raw_dataset <- raw_source[[raw_dataset_name]]
 
-    ds_out1 <- cal_min_max_date(
-      raw_dataset = raw_dataset,
-      sdtm_var_name = der_var,
-      date_variable = date_variable,
-      val_type = min_max
-    )
-    ds_out <- rbind(ds_out, ds_out1)
+    if (der_var == sdtm_var) {
+      ds_out1 <- cal_min_max_date(
+        raw_dataset = raw_dataset,
+        sdtm_var_name = der_var,
+        date_variable = date_variable,
+        time_variable = time_var,
+        date_format = date_format,
+        time_format = time_format,
+        val_type = min_max
+      )
+      ds_out <- rbind(ds_out, ds_out1)
+    }
   }
 browser()
 
-# ref_dates <- purrr::pmap_df(ds_out, .f = get_df)
+#ref_dates <- purrr::pmap_df(ds_out, .f = get_df)
 if (min_max == "min") {
-  df_final <- ds_out %>% dplyr::arrange(SUBJID,datetime)
+  df_final <- ds_out %>% dplyr::arrange(patient_number,datetime)
 } else {
-  df_final <- ds_out %>% dplyr::arrange(dplyr::desc(SUBJID),dplyr::desc(datetime))
+  df_final <- ds_out %>% dplyr::arrange(dplyr::desc(patient_number),dplyr::desc(datetime))
 }
-df_final <- df_final[!duplicated(df_final$SUBJID), c("SUBJID", "datetime")]
+df_final <- df_final[!duplicated(df_final$patient_number), c("patient_number", "datetime")]
 colnames(df_final)[colnames(df_final) == "datetime"] <- der_var
 
-return(df_final)
+dm <- dplyr::left_join(ds_in, y = df_final, by = "patient_number")
+return(dm)
 
 }
